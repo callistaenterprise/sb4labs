6dc4e9024938f3f8412b49cb7e962398f1819ce0
Magnus Larsson <magnus.larsson@callistaenterprise.se>
2026-01-19 12:04:16 +0100

    Got SC CB to work with proper exception handling + added logging of CB events

--- api-consumer/src/main/java/se/magnus/sb4labs/apiconsumer/interfaceclients/ProductFallbacks.java
+++ api-consumer/src/main/java/se/magnus/sb4labs/apiconsumer/interfaceclients/ProductFallbacks.java
@@ -1,23 +1,42 @@
 package se.magnus.sb4labs.apiconsumer.interfaceclients;
 
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.client.HttpClientErrorException;
 import se.magnus.sb4labs.api.core.product.Product;
-import se.magnus.sb4labs.apiconsumer.ProductCompositeRestController;
+import se.magnus.sb4labs.api.exceptions.NotFoundException;
 
 public class ProductFallbacks {
 
   final static private Logger LOG = LoggerFactory.getLogger(ProductFallbacks.class);
 
-  public Product getProduct(int productId) {
-    LOG.warn("getProduct({}) fallback invoked", productId);
-    return (new Product(productId, "Fallback product" + productId, productId));
-  }
+  // TODO: CallNotPermittedException does not seem to work?
+  // public Product getProduct(CallNotPermittedException cause, int productId, int delay, int faultPercent) {
+  public Product getProduct(Throwable cause, int productId, int delay, int faultPercent) {
 
-  // Optional: inspect the failure cause
-  public Product getProduct(Throwable cause, int productId) {
-    LOG.warn("getProduct({}) fallback invoked, caused by {}", productId, cause.getMessage());
-    LOG.warn("getProduct({}) fallback invoked", productId, cause);
-    return (new Product(productId, "Fallback product" + productId, productId));
+    switch (cause) {
+      case CallNotPermittedException _:
+        // TODO: Extract to separate class...
+        LOG.warn("getProduct({}) fallback invoked, caused by {}", productId, cause.toString());
+
+        if (productId == 13) {
+          String errMsg = "Product Id: " + productId + " not found in fallback cache!";
+          LOG.warn(errMsg);
+          throw new NotFoundException(errMsg);
+        }
+
+        return (new Product(productId, "Fallback product" + productId, productId));
+
+      case RuntimeException rtex:
+        LOG.warn("Rethrowing a RuntimeException");
+        throw rtex;
+
+      default:
+        LOG.warn("Rethrowing a checked exception wrapped in a RuntimeException {}", cause.getClass().getName());
+        LOG.warn("Error message: {}", cause.getMessage());
+        throw new RuntimeException(cause);
+    }
+
   }
 }
